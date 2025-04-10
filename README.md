bd1a4f1fbb9f24d5ea4f8de31443a03be2a545a2
Johnny McGee <johnnymcgee.dev@gmail.com>
2025-04-10 09:43:32 -0500

    Update config defaults

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-# Update GraphQL Schema
+# Usage
+## Install
+```sh
+go install github.com/JohnnyMcGee/metadef
+```
+
+## Config
+Configuration is done using a hjson file. By default, the CLI looks for a global config file at `$HOME/.metadef.hjson`. You can pass a local config file using the flag `metadef -c /path/to/config.hjson`.
+
+```hjson
+{
+// Map shop domain to admin access token. There must be at least one shop configured.
+  shops: {
+    shop-domain-1: shopify-admin-api-access-token-1
+    shop-domain-2: shopify-admin-api-access-token-2
+    shop-domain-3: shopify-admin-api-access-token-3
+  }
+// Optionally specify the Shopify Admin API version to use.
+  version: 2025-04
+}
+```
+
+### `shops`
+`shops` is a map from shop domain to admin api access token. This enables the CLI to access each of your shops.
+
+#### Shop Domain
+The CLI uses Shopify Admin GraphQL API. To gain access to your store, add an entry to the `shops` map. The key should be your myshopify domain (without the .myshopify.com suffix). For example, if your store is `super-awesome-store.myshopify.com`, the key will be `super-awesome-store`.
+
+#### Access Token
+The easiest way to get an admin access token is to [create a custom shopify app](https://help.shopify.com/en/manual/apps/app-types/custom-apps).
+
+Your app will need read/write permissions for metaobject definitions and metaobjects.
+
+# Development
+## Update GraphQL Schema
 Periodically it may be necessary to fetch the latest schema for Shopify Admin GraphQL API. One way to do this is using the [get-graphql-schema CLI tool](https://github.com/gqlgo/get-graphql-schema).
 
 1. Install get-grapqhl-schema tool
--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -16,11 +17,13 @@ import (
 	"github.com/sergi/go-diff/diffmatchpatch"
 )
 
-const configFile = ".metaobjectsrc.hjson"
+const DEFAULT_API_VERSION = "2025-04"
 
 var (
-	shop    string
-	outFile string
+	shop       string
+	outFile    string
+	configFile string
+	config     Config
 )
 
 type Config struct {
@@ -29,14 +32,15 @@ type Config struct {
 }
 
 func ReadConfig(path string) (Config, error) {
+
 	b, err := os.ReadFile(path)
 	if err != nil {
-		return Config{}, err
+		return Config{}, errors.New("Error reading config file: " + path)
 	}
 
 	var config Config
 	if err := hjson.Unmarshal(b, &config); err != nil {
-		return Config{}, err
+		return Config{}, errors.New("Error unmarshalling config file: " + path)
 	}
 
 	return config, nil
@@ -59,15 +63,40 @@ func Execute() {
 }
 
 func init() {
+	rootCmd.PersistentFlags().StringVarP(&configFile, "config", "c", "", "Config file path")
 	rootCmd.PersistentFlags().StringVarP(&shop, "shop", "s", "", "Shopify shop domain (without the .myshopify.com extension)")
-	rootCmd.MarkFlagRequired("shop")
 	rootCmd.PersistentFlags().StringVarP(&outFile, "out", "o", "", "Output file name")
-
-	diffCmd.MarkFlagRequired("file")
 
 	rootCmd.AddCommand(pullCmd)
 	rootCmd.AddCommand(diffCmd)
 	rootCmd.AddCommand(pushCmd)
+}
+
+func initDefaults() {
+	if configFile == "" {
+		configFile = os.Getenv("HOME") + "/.metadef.hjson"
+	}
+
+	var err error
+	config, err = ReadConfig(configFile)
+	if err != nil {
+		log.Fatalf("Error reading config: %v\n", err)
+	}
+
+	if len(config.Shops) == 0 {
+		log.Fatalf("Error: No shops found in config file: %s\n", configFile)
+	}
+
+	if shop == "" {
+		for key := range config.Shops {
+			shop = key
+			break
+		}
+	}
+
+	if config.Version == "" {
+		config.Version = DEFAULT_API_VERSION
+	}
 }
 
 var pushCmd = &cobra.Command{
@@ -75,12 +104,9 @@ var pushCmd = &cobra.Command{
 	Short: "Push local metaobject definitions to the Shopify store",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		config, err := ReadConfig(configFile)
-		if err != nil {
-			log.Fatalf("Error reading config: %v\n", err)
-			return err
-		}
-
+		initDefaults()
+		log.Printf("Using config file %s\n", configFile)
+		log.Printf("Pushing definitions from file %s to shop %s\n", args[0], shop)
 		client := shopify.NewShopifyAdminClient(shop, config.Shops[shop], config.Version)
 		ms := &core.MetaobjectService{ShopifyClient: &client}
 
@@ -101,11 +127,9 @@ var diffCmd = &cobra.Command{
 	Short: "Compare local metaobject definitions with the Shopify store",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		config, err := ReadConfig(configFile)
-		if err != nil {
-			log.Fatalf("Error reading config: %v\n", err)
-			return err
-		}
+		initDefaults()
+		log.Printf("Using config file %s\n", configFile)
+		log.Printf("Diffing metaobject definitions from file %s to shop %s\n", args[0], shop)
 
 		client := shopify.NewShopifyAdminClient(shop, config.Shops[shop], config.Version)
 		ms := &core.MetaobjectService{ShopifyClient: &client}
@@ -152,11 +176,9 @@ var pullCmd = &cobra.Command{
 	Use:   "pull",
 	Short: "Pull all metaobject definitions from the Shopify store",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		config, err := ReadConfig(configFile)
-		if err != nil {
-			log.Fatalf("Error reading config: %v\n", err)
-			return err
-		}
+		initDefaults()
+		log.Printf("Using config file %s\n", configFile)
+		log.Printf("Pulling metaobject definitions from shop %s\n", shop)
 
 		client := shopify.NewShopifyAdminClient(shop, config.Shops[shop], config.Version)
 		ms := &core.MetaobjectService{ShopifyClient: &client}
